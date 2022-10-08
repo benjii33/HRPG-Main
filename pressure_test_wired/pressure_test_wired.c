4d3913ddef8aa2b5ed94803253042c1ddd47c5e9
Ben Fichtenkort <benfich000@gmail.com>
2022-10-08 17:07:58 -0400

    Added loop timing code to pressure test

--- pressure_test_wired/pressure_test_wired.c
+++ pressure_test_wired/pressure_test_wired.c
@@ -3,6 +3,7 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/spi.h"
+#include "pico/time.h"
 
 #define PIN_MISO 4
 #define PIN_CS   5
@@ -41,7 +42,8 @@ uint16_t mcp0Read(uint adc_chan) {
 int main() {
     stdio_init_all();
     
-    // Initialize pin 15 as relay control
+    // Initialize pin 15 as relay control.
+    // Important to do this step early so solenoid outputs are well-behaved
     gpio_init(15);
     gpio_set_dir(15, 1);
     gpio_put(15, 0);
@@ -65,7 +67,15 @@ int main() {
     uint16_t sample1;
     uint16_t sample2;
 
+
+    uint32_t time_ms = 0;
+
+    absolute_time_t timeout_time;
+
+    // 1ms loop
     while (1) {
+        timeout_time = make_timeout_time_us(1000); // 1000 us (1 ms) loop time
+
         sample2 = mcp0Read(1);
         //sample2 = mcp0Read(2);
 
@@ -83,10 +93,17 @@ int main() {
 
         // printf("Pressure 1: %f, Pressure 2: %f\n", pressure1, pressure2);
 
-        sleep_ms(20);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(20);
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+
+        // 20ms on, 20ms off blink cycle
+        if(time_ms%40 < 20) {
+            gpio_put(PICO_DEFAULT_LED_PIN, 1); // blink
+        } else {
+            gpio_put(PICO_DEFAULT_LED_PIN, 0); // blonk
+        }
+
+        // Wait until 1 ms loop time has passed
+        sleep_until(timeout_time);
+        time_ms++;
     }
 
     return 0;
